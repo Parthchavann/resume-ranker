934d6f52636c3fd591afb28ec80306f77f381882
Parthchavann <120365789+Parthchavann@users.noreply.github.com>
2025-07-08 19:14:18 -0500

    Update .eslintrc.cjs

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,1 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+};
 
