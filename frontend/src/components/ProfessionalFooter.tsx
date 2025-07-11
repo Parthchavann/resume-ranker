80c0170989ac041a049953adaafdc58ea575cbc3
Parthchavann <120365789+Parthchavann@users.noreply.github.com>
2025-07-11 15:27:57 -0500

    Update ProfessionalFooter.tsx

--- frontend/src/components/ProfessionalFooter.tsx
+++ frontend/src/components/ProfessionalFooter.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ProfessionalFooter from '../components/ProfessionalFooter';
 import { motion } from "framer-motion";
 import { ShieldCheck, Trophy, Handshake, Sparkles } from "lucide-react";
 
